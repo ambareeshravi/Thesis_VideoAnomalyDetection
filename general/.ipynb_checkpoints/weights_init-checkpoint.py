1e3d0fd01d8bbbc10abe710e7102d5b3e57e0ecc
ambareeshravi <ambareeshvichuravi@hotmail.com>
2020-11-25 22:58:44 -0500

    updated weights_init

--- general/.ipynb_checkpoints/weights_init-checkpoint.py
+++ general/.ipynb_checkpoints/weights_init-checkpoint.py
@@ -1,3 +1,5 @@
+from torch import nn
+
 def weights_init(m, initializer_type = "kaiming_uniform"):
     '''
     Initializes the weigts of the network
@@ -15,9 +17,9 @@ def weights_init(m, initializer_type = "kaiming_uniform"):
             init_fn = nn.init.xavier_normal_
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv3d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.ConvTranspose3d):
         init_fn(m.weight.data)
-    if m.bias:
-        init_fn(m.bias)
-        init_fn(m.bias.data)
-    if isinstance(m, nn.BatchNorm2d):
-        init_fn(m.weight.data, 1.0, 0.02)
-        init_fn(m.bias.data, 0)
+#     if m.bias:
+#         init_fn(m.bias)
+#         init_fn(m.bias.data)
+#     if isinstance(m, nn.BatchNorm2d):
+#         init_fn(m.weight.data, 1.0, 0.02)
+#         init_fn(m.bias.data, 0)
