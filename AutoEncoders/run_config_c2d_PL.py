db9b3c8325fe160c897f0453c49e8638a17989dc
ambareeshravi <ambareeshvichuravi@hotmail.com>
2020-12-01 18:21:11 -0500

    added pytorch lightning support for DDP training

---
+++ AutoEncoders/run_config_c2d_PL.py
@@ -0,0 +1,155 @@
+import argparse
+import sys
+sys.path.append("..")
+
+from general import *
+
+# Import AutoEncoder trainer and tester scripts
+from train_autoencoder_PL import *
+from test_autoencoder import *
+
+# Import all available models
+from C2D_Models import *
+from PatchWise.models_PatchWise import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Parameters to run the training")
+    parser.add_argument("--data_path", type = str, help="Path to read data from")
+    parser.add_argument("--model_path", type = str, help="Path to store the models")
+    parser.add_argument("--nodes", type = int, default = 1, help = "Number of GPU nodes")
+    args = parser.parse_args()
+    
+    # Editable
+    IMAGE_SIZE = 128
+    EPOCHS = 300
+    BATCH_SIZE = 128
+    IMAGE_TYPE = "normal"
+    MODEL_PATH = args.model_path
+    create_directory(MODEL_PATH)
+    DATA_PATH = os.path.join(args.data_path, "VAD_Datasets")
+    OPTIMIZER_TYPE = "adam"
+    LOSS_TYPE = "mse"
+    DENOISING = False
+    STACKED = False
+    PATCH_WISE = False
+    
+    isVideo = False
+    stackFrames = 1
+    asImages = True
+    if isVideo:
+        stackFrames = 16
+        asImages = False
+        
+    # Manual
+    DATA_TYPE = "ucsd2" 
+    
+    # PL Params
+    PRECISION = 32 #16
+    GPUS = -1
+    GRAD_CLIP_VAL = 0
+    NODES = args.nodes
+    
+    # 1. Training
+    train_data, CHANNELS = select_dataset(
+        dataset = DATA_TYPE,
+        parent_path = DATA_PATH,
+        isTrain = True,
+        asImages = asImages,
+        image_size = IMAGE_SIZE,
+        image_type = IMAGE_TYPE,
+        n_frames = 16,
+        frame_strides = [1,2,4,8,16],
+        sample_stride = 1,
+    )
+
+    train_loader, val_loader = get_data_loaders(train_data, batch_size = BATCH_SIZE)
+    INFO("TRAINING DATA READY")
+    
+    # Manual
+    model = C2D_AE_128_3x3(channels = CHANNELS)
+    
+    model_file = "PL_%s_%s_%s_%s_E%03d_BS%03d"%(model.__name__, IMAGE_TYPE, OPTIMIZER_TYPE, LOSS_TYPE, EPOCHS, BATCH_SIZE)
+    if DENOISING: model_file += "_DeNoising"
+    if STACKED: model_file += "_Stacked"
+    if PATCH_WISE: model_file += "_PatchWise"
+        
+    MODEL_SAVE_PATH = os.path.join(MODEL_PATH, model_file)
+    create_directory(MODEL_SAVE_PATH)
+    lm_model = AutoEncoderLM(
+        model,
+        MODEL_SAVE_PATH,
+        LOSS_TYPE,
+        OPTIMIZER_TYPE,
+        default_learning_rate = 1e-3,
+        max_epochs = 300,
+        status_rate = 25,
+        lr_scheduler_kwargs = {
+            'factor': 0.75,
+            'patience': 5,
+            'threshold': 1e-4,
+            'verbose': True
+         }
+    )
+    INFO("MODEL, OPTIM, LOSS ARE READY")
+    
+    # Automated
+    callbacks_list = [
+        EpochChange(),
+#         EarlyStopping('validation_loss', min_delta = 1e-8, patience=15, verbose=True)
+    ]
+    
+    trainer = Trainer(
+        default_root_dir = MODEL_SAVE_PATH,
+        gpus=GPUS,
+        nodes = NODES,
+        accelerator='ddp',
+        min_epochs = 50,
+        max_epochs = EPOCHS,
+        gradient_clip_val = GRAD_CLIP_VAL,
+        precision = PRECISION,
+        callbacks = callbacks_list,
+        progress_bar_refresh_rate = 0,
+        weights_summary = None,
+        auto_lr_find=True
+    )
+    trainer.tune(model = lm_model, train_dataloader = train_loader)
+    INFO("STARTING THE TRAINING")
+    trainer.fit(model = lm_model, train_dataloader=train_loader, val_dataloaders=val_loader)
+
+    # 2. Testing
+    test_data, CHANNELS = select_dataset(
+        dataset = DATA_TYPE,
+        parent_path = DATA_PATH,
+        isTrain = False,
+        asImages = True,
+        image_size = IMAGE_SIZE,
+        image_type = IMAGE_TYPE,
+        n_frames = 16,
+        frame_strides = [1,2,4,8,16],
+        sample_stride = 1,
+    )
+    INFO("TESTING DATA READY")
+    
+    # Changed for PL module
+    ae_model = trainer.model
+    print("-"*40)
+    print(ae_model.model_file)
+    tester = AutoEncoder_Tester(
+        model = ae_model.model,
+        dataset = test_data,
+        patchwise = PATCH_WISE,
+        stacked = STACKED,
+        useGPU = True
+    )
+    results = tester.test(
+        stackFrames = stackFrames,
+        isVideo = isVideo,
+        save_as = ".pkl".join(ae_model.model_file.split(".pth.tar"))
+    )
+    print("-"*40)
+    try:
+        del trainer, tester
+        gc.collect()
+    except:
+        pass
+    INFO("** Sucessfully Completed Execution **")
---
+++ AutoEncoders/train_autoencoder_PL.py
@@ -0,0 +1,151 @@
+import sys
+sys.path.append("..")
+from general import *
+from general.data import *
+from general.all_imports import *
+from general.pl_callbacks import EpochChange
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+
+class AutoEncoderLM(LightningModule):
+    def __init__(self,
+                 model,
+                 save_path,
+                 loss_type,
+                 optimizer_type,
+                 default_learning_rate = 1e-3,
+                 max_epochs = 300,
+                 status_rate = 25,
+                 lr_scheduler_kwargs = {
+                     'factor': 0.75,
+                     'patience': 5,
+                     'threshold': 1e-4,
+                     'verbose': True
+                 }
+                ):
+        super().__init__()
+        # General params
+        self.learning_rate = default_learning_rate
+        self.model = model
+        self.loss_criterion = select_loss[loss_type]
+        self.optimizer_type = optimizer_type
+        self.status_rate = status_rate
+        self.EPOCH = 0
+        self.MAX_EPOCHS = max_epochs
+        self.lr_scheduler_kwargs = lr_scheduler_kwargs
+        
+        # Path
+        self.save_path = save_path
+        if not os.path.exists(self.save_path): os.mkdir(self.save_path)
+        self.model_file = join_paths([self.save_path, os.path.split(self.save_path)[-1] + ".pth.tar"])
+        
+        # Model Params
+        self.history = {
+            "train_loss": list(),
+            "validation_loss": list()
+        }
+        
+        self.epoch_train_loss = list()
+        self.epoch_validation_loss = list()
+
+        if "vae" in self.model_file.lower():
+            self.step = self.vae_step
+        self.addNoise = False
+        if "nois" in self.model_file.lower():
+            self.addNoise = True
+        if "patch" in self.model_file.lower():
+            self.step = self.patch_step
+        if "stack" in self.model_file.lower():
+            self.step = self.stack_step
+        
+    def epoch_reset(self,):
+        train_loss = np.mean(self.epoch_train_loss)
+        val_loss = np.mean(self.epoch_validation_loss)
+        self.history["train_loss"].append(train_loss)
+        self.history["validation_loss"].append(val_loss)
+        self.epoch_train_loss = list()
+        self.epoch_validation_loss = list()
+        
+    def epoch_status(self,):
+        print("="*60)
+        print("Epoch: [%03d/%03d] | Time: %0.2f (s) | Model: %s"%(self.EPOCH, self.MAX_EPOCHS, (self.EPOCH_END_TIME-self.EPOCH_START_TIME), self.model_file))
+        print("-"*60)
+        try:
+            d = {
+            "Training" : {"Loss -2": self.history["train_loss"][-3],
+                        "Loss -1": self.history["train_loss"][-2],
+                        "Loss *" : self.history["train_loss"][-1],
+                         },
+            "Validation" : {"Loss -2": self.history["validation_loss"][-3],
+                            "Loss -1": self.history["validation_loss"][-2],
+                            "Loss *" : self.history["validation_loss"][-1],
+                         },
+        }
+        except:
+            d = {
+            "Training" : {"Loss" : self.history["train_loss"][-1],},
+            "Validation" : {"Loss" : self.history["validation_loss"][-1],},
+            }
+        print(pd.DataFrame(d).T)
+#         print("="*60)
+        
+    def save(self,):
+        save_model(self.model, self.model_file)
+    
+    def save_final(self,):
+        self.save()
+        plot_stat(self.history, os.path.split(self.model_file)[-1], self.save_path)
+        with open(os.path.join(self.save_path, "train_stats.pkl"), "wb") as f:
+            pkl.dump(self.history, f)
+
+    def get_inputs(self, images):
+        if self.addNoise:
+            return add_noise(images)
+        return images
+
+    def step(self, images):
+        reconstructions, encodings = self.model(self.get_inputs(images))
+        return self.loss_criterion(images, reconstructions)
+
+    def patch_step(self, images):
+        patch_images = get_patches(images)
+        reconstructions, encodings = self.model(patch_images)
+        return self.loss_criterion(patch_images, reconstructions)
+
+    def stack_step(self, images):
+        # images 32x1x16x128x128
+        stacked_images = images.squeeze(dim = -4)
+        reconstructions, encodings = self.model(stacked_images)
+        return self.loss_criterion(stacked_images, reconstructions)
+
+    def vae_step(self, images):
+        reconstructions, latent_mu, latent_logvar = self.model(self.get_inputs(images))
+        return self.vae_loss(images, reconstructions, latent_mu, latent_logvar)      
+    
+    # LM functions
+    def configure_optimizers(self):
+        optimizer = select_optimizer[self.optimizer_type](self.model, lr = self.learning_rate)
+        return {
+           'optimizer': optimizer,
+           'lr_scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau(
+               optimizer,
+               **self.lr_scheduler_kwargs
+           ),
+           'monitor': 'validation_loss'
+       }
+
+    def training_step(self, batch, batch_idx):
+        images, labels = batch
+        train_loss = self.step(images)
+        self.epoch_train_loss.append(train_loss.item())
+        self.log('training_loss', train_loss, on_epoch=True, logger=True, sync_dist=True)
+        return train_loss
+
+    def validation_step(self, batch, batch_idx):
+        images, labels = batch
+        val_loss = self.step(images)
+        self.epoch_validation_loss.append(val_loss.item())
+        self.log('validation_loss', val_loss, on_epoch=True, logger=True, sync_dist=True)
+        return val_loss
