2cddb1f9cc4caac3ca3f4996a85e58e4267dcedb
ambareeshravi <ambareeshvichuravi@hotmail.com>
2020-12-03 22:32:54 -0500

    updated generic slurm_scripts

--- slurm_scripts/VAD_Train_c2d.sh
+++ slurm_scripts/VAD_Train_c2d.sh
@@ -4,37 +4,43 @@
 # ---------------------------------------------------------------------
 #SBATCH --account=def-karray
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:t4:2   # Request GPU "generic resources" [--gres=gpu:2]
-#SBATCH --cpus-per-task=44  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham. [--ntasks-per-node=32]
-#SBATCH --mem=120G        # Memory proportional to GPUs: 31500 Cedar, 63500 Graham. [--mem=127G ]
+#SBATCH --gres=gpu:1   # Request GPU "generic resources" [--gres=gpu:2]
+#SBATCH --cpus-per-task=16  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham. [--ntasks-per-node=32]
+#SBATCH --mem=63G        # Memory proportional to GPUs: 31500 Cedar, 63500 Graham. [--mem=127G ]
 
-#SBATCH --time=0-4:00      # time (DD-HH:MM)
+#SBATCH --time=0-6:00      # time (DD-HH:MM)
+#SBATCH --output=~/projects/def-karray/a24ravi/slurm_outputs/C2D_%u-%x-%j.txt
 
 #SBATCH --mail-user=ambareesh.ravi@uwaterloo.ca
 #SBATCH --mail-type=ALL
 
+version=C2D_$(date +%d_%m_%Y_%H_%M)
+dataset=VAD_Datasets
+project_dir=~/projects/def-karray/a24ravi/
+version_path=$SLURM_TMPDIR/$version/
+mkdir -p $version_path
+echo "[STATUS] Created version: $version"
+
 free -g
 nvidia-smi
 
-tar xf ~/projects/def-karray/a24ravi/VAD_Datasets.tar -C $SLURM_TMPDIR/
+tar xf ${project_dir}${dataset}.tar -C $SLURM_TMPDIR/
 echo "[STATUS] Created data directory"
 
 module load python/3.7.4
 source /home/$USER/ENV/bin/activate
 echo "[STATUS] Python environment ready"
 
-mkdir $SLURM_TMPDIR/Models
 cd ~/workspace/Thesis_VideoAnomalyDetection/AutoEncoders/
 
 echo "[STATUS] Starting script at `date`"
-python run_config_c2d.py --model_path $SLURM_TMPDIR/Models/ --data_path $SLURM_TMPDIR/
+cp run_config_c2d.py $version_path
+python run_config_c2d.py --model_path $version_path --data_path $SLURM_TMPDIR/ --nodes 1
 echo "[STATUS] Script completed at `date`" 
- 
-for d in $SLURM_TMPDIR/Models/*/; do cp run_config_c2d.py "$d"; done
-for d in $SLURM_TMPDIR/Models/*/; do cp slurm-$SLURM_JOB_ID.out "$d"; done
 
-tar cf ~/projects/def-karray/a24ravi/trained_models/`date +%d_%m_%Y_%H_%M.tar` $SLURM_TMPDIR/Models/*
+cp ${project_dir}slurm_outputs/C2D_%u-%x-%j.txt $version_path
+tar -cjf ${project_dir}trained_models/${version}.tar -C $version_path $(ls $version_path)
 echo "[STATUS] Models copied safely"
 
 deactivate
-echo "[STATUS] Deactivate python environment"
+echo "[STATUS] Deactivate python environment. EXITING ..."
