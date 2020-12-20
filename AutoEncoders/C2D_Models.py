c44a784164b39cf679dff9dd315f4b30e6b53602
ambareeshravi <ambareeshvichuravi@hotmail.com>
2020-12-20 17:35:43 -0500

    added squeeze excitation block

--- AutoEncoders/C2D_Models.py
+++ AutoEncoders/C2D_Models.py
@@ -722,6 +722,51 @@ class C2D_AE_128_WIDE(nn.Module):
             
         reconstructions = layer_input
         return reconstructions, encodings
+
+class C2D_AE_128_3x3_SE(nn.Module):
+    def __init__(
+        self,
+        channels = 3,
+        filters_count = [64,64,64,96,96,128],
+        conv_type = "conv2d"
+    ):
+        super(C2D_AE_128_3x3_SE, self).__init__()
+        self.__name__ = "C2D_3x3_128_SE"
+        self.channels = channels
+        self.filters_count = filters_count
+        self.embedding_dim = [1,self.filters_count[4],4,4]
+        
+        self.encoder = nn.Sequential(
+            C2D_BN_A(self.channels, self.filters_count[0], 3, 2, conv_type = conv_type),
+            SE_Block(self.filters_count[0]),
+            C2D_BN_A(self.filters_count[0], self.filters_count[1], 3, 2, conv_type = conv_type),
+            SE_Block(self.filters_count[1]),
+            C2D_BN_A(self.filters_count[1], self.filters_count[2], 3, 2, conv_type = conv_type),
+            SE_Block(self.filters_count[2]),
+            C2D_BN_A(self.filters_count[2], self.filters_count[3], 3, 2, conv_type = conv_type),
+            SE_Block(self.filters_count[3]),
+            C2D_BN_A(self.filters_count[3], self.filters_count[4], 4, 1, conv_type = conv_type, activation_type = "tanh"),
+#             C2D_BN_A(self.filters_count[4], self.filters_count[5], 2, 1, conv_type = conv_type, activation_type = "tanh"),
+        )
+        
+        self.decoder = nn.Sequential(
+#             CT2D_BN_A(self.filters_count[5], self.filters_count[4], 2, 1),
+            CT2D_BN_A(self.filters_count[4], self.filters_count[3], 4, 1),
+            SE_Block(self.filters_count[3]),
+            CT2D_BN_A(self.filters_count[3], self.filters_count[2], 3, 2),
+            SE_Block(self.filters_count[2]),
+            CT2D_BN_A(self.filters_count[2], self.filters_count[1], 3, 2),
+            SE_Block(self.filters_count[1]),
+            CT2D_BN_A(self.filters_count[1], self.filters_count[0], 4, 2),
+            SE_Block(self.filters_count[0]),
+            CT2D_BN_A(self.filters_count[0], self.filters_count[0], 4, 2),
+            C2D_BN_A(self.filters_count[0], self.channels, 3, 1, activation_type = "sigmoid")
+        )
+        
+    def forward(self, x):
+        encodings = self.encoder(x)
+        reconstructions = self.decoder(encodings)
+        return reconstructions, encodings    
     
 C2D_MODELS_DICT = {
     128: {
@@ -749,6 +794,9 @@ C2D_MODELS_DICT = {
         },
         "wide": {
             "3x3": C2D_AE_128_WIDE
+        },
+        "squeeze_excitation": {
+            "3x3": C2D_AE_128_3x3_SE
         }
     },
     
