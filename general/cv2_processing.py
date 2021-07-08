180d859881de04b002d9b3a8b47dae2fdd88158e
Ambareesh Ravi <ambareeshvichuravi@hotmail.com>
2021-07-08 17:26:02 -0400

    Added documentation

--- general/cv2_processing.py
+++ general/cv2_processing.py
@@ -1,17 +1,57 @@
+'''
+Contains OpenCV functions for image and video input processing
+'''
+
+# imports
 import numpy as np
 np.random.seed(0)
 
 import cv2
 
 class OpticalFlow:
+    # Class to obtain the optical flow between two frames
     def __init__(self, returnMag = True):
+        '''
+        Description:
+            Initiates the class
+        
+        Args:
+            returnMag: <bool> returns the optical flow with only magnitude in 1 channel if true or includes
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.returnMag = returnMag
     
     def convert_to_gray(self, image):
+        '''
+        Description:
+            Converts color image to grayscale
+        
+        Args:
+            image: input image as <np.array>
+        Returns:
+            grayscale image as <np.array>
+        Exception:
+            -
+        '''
         if image.shape[-1]!=3: return image
         return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     def get_optical_flow(self, frame1, frame2):
+        '''
+        Description:
+            calculates the optical flow between two frames
+        
+        Args:
+            frame1: input frame 1 as <np.array>
+            frame2: input frame 2 as <np.array>
+        Returns:
+            returns the optical flow as <np.array>
+        Exception:
+            -
+        '''
         hsv = np.zeros_like(frame1)
         hsv[...,1] = 255
         p_frame = self.convert_to_gray(frame1)
@@ -29,11 +69,34 @@ class OpticalFlow:
         return rgb_flow # returns 3 channel flow
     
 class BackgroundSubtraction:
+    # Subtract the background from the input image given a reference image
     def __init__(self):
+        '''
+        Description:
+            Initiates the class
+        
+        Args:
+            -
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.fgbg = cv2.createBackgroundSubtractorMOG2()
         self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
         
     def __call__(self, frame):
+        '''
+        Description:
+            object call to 
+        
+        Args:
+            frame: input image <np.array>
+        Returns:
+            foreground mask as <np.array>
+        Exception:
+            -
+        '''
         fgmask = self.fgbg.apply(frame)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, self.kernel)
         return fgmask
