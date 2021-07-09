a2051c1bc8113cbfc141e6f305edd92b9173e130
Ambareesh Ravi <ambareeshvichuravi@hotmail.com>
2021-07-08 20:59:39 -0400

    Added documentation

--- general/losses.py
+++ general/losses.py
@@ -1,3 +1,6 @@
+# Module contains the implementations of differnet loss functions
+
+# imports
 from .all_imports import *
 
 import torch.nn.functional as F
@@ -8,25 +11,95 @@ from pytorch_msssim import MS_SSIM, ms_ssim, SSIM, ssim
     
 class MSE_LOSS:
     def __init__(self, reduction = "mean"):
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            reduction - reduction type as <str> - sum/mean
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.loss = nn.MSELoss(reduction = reduction)
     
     def __call__(self, original, reconstructions):
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         return self.loss(original, reconstructions)
 
 class BCE_LOSS:
     def __init__(self, reduction = "mean"):
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            reduction - reduction type as <str> - sum/mean
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.loss = nn.BCELoss(reduction = reduction)
     
     def __call__(self, original, reconstructions):
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         return self.loss(reconstructions, original)
 
 class CONTRACTIVE_LOSS:
     def __init__(self, primary_loss = "mse", lamda = 1e-3):
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            primary_loss - type of the primary loss to be used with contractive loss as <str>
+            lambda - lambda value for contractive loss as <float>
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.main_loss = MSE_LOSS(reduction = "mean")
         if "bce" in primary_loss: self.main_loss = BCE_LOSS(reduction = "mean")
         self.lamda = lamda
     
     def __call__(self, original, reconstructions, encodings, W):
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         main_loss = self.main_loss(original, reconstructions)
 #         W = torch.flatten(W, start_dim = 1, end_dim = -1)
 #         dh = (encodings * (1 - encodings)).to(Config.device)
@@ -37,9 +110,32 @@ class CONTRACTIVE_LOSS:
     
 class PSNR_LOSS:
     def __init__(self, limit = 100):
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            limit - max value for PSRN loss as <float> - usually 1 or 100
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.limit = limit
     
     def __call__(self, original, reconstructions):
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         mse = torch.mean((original - reconstructions)**2)
         if mse == 0: psnr = self.limit
         else: psnr =  (10 * torch.log10(1. / mse)) * (self.limit / 100)
@@ -47,15 +143,51 @@ class PSNR_LOSS:
     
 class VARIATIONAL_LOSS:
     def __init__(self):
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            -
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.bce_loss = BCE_LOSS(reduction = "mean")
         
     def __call__(self, original, reconstructions, mu, logvar):
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+            mu - mu for variational loss as <torch.Tensor>
+            logvar - logvar for variational loss as <torch.Tensor>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         BCE = self.bce_loss(original, reconstructions)
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         return BCE + KLD, {"BCE": BCE, "KLD": KLD}
     
 class WEIGHTED_SIMILARITY:
     def __init__(self, primary_loss = "mse", weights = [1.0, 1.0], asImages = True): # equal weightage 1,1 is same as 0.5, 0.5
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            limit - max value for PSRN loss as <float> - usually 1 or 100
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.main_loss = MSE_LOSS(reduction = "mean")
         if "bce" in primary_loss: self.main_loss = BCE_LOSS(reduction = "mean")
         self.ssim_loss = SSIM(data_range = 1.0, nonnegative_ssim=True)
@@ -63,6 +195,9 @@ class WEIGHTED_SIMILARITY:
         self.asImages = asImages
         
     def transform(self, x):
+        '''
+        Transforms the input to compatible format for the loss
+        '''
         # Gray Image to 3 channels in images
         if self.asImages and x.shape[-3] == 1: x = x.repeat(1, 3, 1, 1)
         # Gray frames to 3 channels in video
@@ -73,18 +208,65 @@ class WEIGHTED_SIMILARITY:
         else: return x.transpose(1,2).flatten(start_dim = 0, end_dim = 1)
     
     def __call__(self, original, reconstructions, lambda_ = 1): # lambda_ [1,100] pick for training
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+            lambda_ - lambda value for weighted similarity to weight SSIM loss as <float>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         return (self.weights[0] * self.main_loss(original, reconstructions)) + (self.weights[1] * lambda_ * (1 - self.ssim_loss.forward(self.transform(original), self.transform(reconstructions))))
     
 class QUALITY_LOSS(WEIGHTED_SIMILARITY):
     def __init__(self):
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            -
+        Returns:
+            -
+        Exception:
+            -
+        '''
         WEIGHTED_SIMILARITY.__init__(self)
         self.psnr = PSNR_LOSS(limit = 1)
         
     def __call__(self, original, reconstructions):
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         return self.psnr(original, reconstructions) + (1 - self.ssim_loss.forward(self.transform(original), self.transform(reconstructions)))
             
 class MahalanobisLayer(nn.Module):
     def __init__(self, dim = 300):
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            dim - number of dimensions of the encodings
+        Returns:
+            -
+        Exception:
+            -
+        '''
         super(MahalanobisLayer, self).__init__()
         self.register_buffer('S', torch.eye(dim))
         self.register_buffer('S_inv', torch.eye(dim))
@@ -94,6 +276,18 @@ class MahalanobisLayer(nn.Module):
         return 1 / (x.size(0) - 1) * x.t().mm(x)
 
     def forward(self, x):
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         delta = x - torch.mean(x, dim = 0)
         self.S_inv = torch.pinverse(self.cov(delta + 1e-10))
         m = torch.mm(torch.mm(delta, self.S_inv), delta.t())
@@ -101,20 +295,46 @@ class MahalanobisLayer(nn.Module):
 
 class MANIFOLD_LOSS:
     def __init__(self, primary_loss = "mse", weights = [1.0, 1.0]):
+        '''
+        Description:
+            Initializes the class
+            
+        Args:
+            primary_loss - type of the primary loss to be used with contractive loss as <str>
+            weights - weight values for MSE and Mahalanobils as <list> - usually equal weightage 
+        Returns:
+            -
+        Exception:
+            -
+        '''
         self.main_loss = MSE_LOSS(reduction = "mean")
         if "bce" in primary_loss: self.main_loss = BCE_LOSS(reduction = "mean")
         self.mahalanobis_loss = MahalanobisLayer()
         self.weights = weights
     
     def __call__(self, original, reconstruction, encoding):
+        '''
+        Description:
+            Object call to calculate the loss
+            
+        Args:
+            original - original image as <torch.Tensor>
+            reconstruction - reconstructed image as <torch.Tensor>
+        Returns:
+            loss value as <torch.Tensor>
+        Exception:
+            -
+        '''
         return (self.weights[0] * self.main_loss(original, reconstruction)) + (self.weights[1] * self.mahalanobis_loss(encoding))
     
 def max_norm(w, max_clip):
+    # implementation of max norm for regularization
     norms = torch.norm_except_dim(w, dim=0).flatten()
     desired = torch.clip(norms, 0, max_clip)
     w *= (desired / (1e-10 + norms))
     return w
-    
+
+# Dict to get the loss function
 select_loss = {
     "mse": MSE_LOSS(),
     "bce": BCE_LOSS(),
